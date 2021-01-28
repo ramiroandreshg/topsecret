f06088688c9bc877091383352e7436bf09e0b256
Ramiro <ramiroandreshg@gmail.com>
2021-01-28 20:33:24 -0300

    added one last tescase for cleanup feature

--- tests/unit/memory-repository.test.js
+++ tests/unit/memory-repository.test.js
@@ -3,8 +3,8 @@ const MemoryRepository = require('../../src/topsecret-msg/memory-repository');
 const memoryRepository = new MemoryRepository();
 
 describe('Memory Repository suite case', () => {
-  beforeEach(() => {
-    memoryRepository.cleanUpSatellitesData();
+  beforeEach(async () => {
+    await memoryRepository.cleanUpSatellitesData();
   });
 
   it('should start with a clean repository if no satellite data has been provided', async () => {
@@ -48,5 +48,19 @@ describe('Memory Repository suite case', () => {
     const { distance } = persistedData[0];
     expect(distance).not.toBe(10.2);
     expect(distance).toBe(200);
+  });
+
+  it('should remove all existent data when performing a clean up', async () => {
+    const satelliteData = { name: 'kenobi', distance: 10.2, message: ['simple', 'message'] };
+    await memoryRepository.saveSingleSatellitesData(satelliteData);
+
+    let persistedData = await memoryRepository.getAllSatellitesData();
+    expect(persistedData.length).toBe(1);
+
+    await memoryRepository.cleanUpSatellitesData();
+    persistedData = await memoryRepository.getAllSatellitesData();
+
+    expect(persistedData.length).toBe(0);
+    expect(persistedData).toEqual([]);
   });
 });
