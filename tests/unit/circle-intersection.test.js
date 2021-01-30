93ae6d3e9aea3a9fc424809a3a8ed3e9f8066c18
Ramiro <ramiroandreshg@gmail.com>
2021-01-30 12:51:21 -0300

    refactor in circle intersection algorithm

--- tests/unit/circle-intersection.test.js
+++ tests/unit/circle-intersection.test.js
@@ -1,10 +1,34 @@
 const { findIntersection } = require('../../src/topsecret-msg/algorithms/v1/circle-intersection');
 
 describe('Find circle intersection unit tests', () => {
-  it('should return an empty result if there is no possible intersection', () => {
-    const c1 = [0, 0, 1];
-    const c2 = [10, 10, 1];
+  it('should return no intersection if circles are too far away from eachother', () => {
+    const c1 = { x: 0, y: 0, d: 1 };
+    const c2 = { x: -10, y: 10, d: 1 };
     const intersection = findIntersection(c1, c2);
     expect(intersection).toEqual([]);
+  });
+
+  it('should return no intersection if one circle is inside the other', () => {
+    const c1 = { x: 0, y: 0, d: 10 };
+    const c2 = { x: 0, y: 0, d: 1 };
+    const intersection = findIntersection(c1, c2);
+    expect(intersection).toEqual([]);
+  });
+
+  it('should return a single intersection point if circle are tangent', () => {
+    const c1 = { x: 0, y: 0, d: 2 };
+    const c2 = { x: 3, y: 0, d: 1 };
+    const intersection = findIntersection(c1, c2);
+    expect(intersection).toEqual([{ x: 2, y: 0 }]);
+  });
+
+  it('should return a both intersection points if in regular intersection scenario', () => {
+    const c1 = { x: -1, y: 1, d: 2 };
+    const c2 = { x: 0, y: 0, d: 1 };
+    const intersection = findIntersection(c1, c2);
+    expect(intersection).toEqual([
+      { x: 0.911, y: 0.411 },
+      { x: -0.411, y: -0.911 },
+    ]);
   });
 });
